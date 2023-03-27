6c3f09d83b75d45b69cf2c70021a727d57a00cb7
Siena Hunsaker <sienahunsaker@gmail.com>
2023-03-26 18:50:05 -0600

    Remove starter code and added riddle page

--- src/pages/riddle.tsx
+++ src/pages/riddle.tsx
@@ -1,21 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Riddle.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Riddle() {
+    const name = "Danny";
   return (
     <>
       <Head>
-        <title>Riddles completed</title>
-        <meta name="description" content="Riddles completed" />
+        <title>Your Riddle</title>
+        <meta name="description" content="Solve this if you're strong enough" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-       <div>Groomsmen</div>
+       <div>Riddle page for {name}</div>
+
       </main>
     </>
   )
